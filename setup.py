73d2b8a2a6a716f6c9c2d0477c1212a7066e0d52
zdaoguang <57522071+zdaoguang@users.noreply.github.com>
2025-04-17 22:34:33 +0800

    update: setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import find_packages, setup
 
 setup(
     name="multi-swe-bench",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=requirements,
     author="Daoguang Zan",
     author_email="zandaoguang@bytedance.com",
     description="Multi-SWE-bench: A Multilingual Benchmark for Issue Resolving",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
+    url="https://github.com/multi-swe-bench/multi-swe-bench",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
