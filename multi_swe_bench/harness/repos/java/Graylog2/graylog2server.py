6f0f225124c06cb99e256ce82e9869a895c8f6f7
zdaoguang <57522071+zdaoguang@users.noreply.github.com>
2025-05-29 16:44:25 +0800

    update: 5 java repos

--- multi_swe_bench/harness/repos/java/Graylog2/graylog2server.py
+++ multi_swe_bench/harness/repos/java/Graylog2/graylog2server.py
@@ -54,7 +54,7 @@ ENV DEBIAN_FRONTEND=noninteractive
 ENV LANG=C.UTF-8
 ENV LC_ALL=C.UTF-8
 WORKDIR /home/
-RUN apt-get update && apt-get install -y git openjdk-17-jdk
+RUN sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list && apt-get update && apt-get install -y git openjdk-17-jdk
 
 {code}
 
