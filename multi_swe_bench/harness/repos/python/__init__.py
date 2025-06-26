f19de2a4ef83b5824d74e6d794e1cb5ecf013ff1
aoyanli <aoyanli.722@bytedance.com>
2025-06-26 17:03:56 +0800

    Revert "Add pydantic package support and update python __init__.py"

--- multi_swe_bench/harness/repos/python/__init__.py
+++ multi_swe_bench/harness/repos/python/__init__.py
@@ -14,4 +14,3 @@ from multi_swe_bench.harness.repos.python.spyderide import *
 from multi_swe_bench.harness.repos.python.napari import *
 from multi_swe_bench.harness.repos.python.mnetools import *
 from multi_swe_bench.harness.repos.python.facebookresearch import *
-from multi_swe_bench.harness.repos.python.pydantic import *
