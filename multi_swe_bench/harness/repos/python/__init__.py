a252522c4b1a4b478764a09a22b8a31bbc381b76
zdaoguang <57522071+zdaoguang@users.noreply.github.com>
2025-07-09 13:23:38 +0800

    add: python repos by EnvAgent (#35)

--- multi_swe_bench/harness/repos/python/__init__.py
+++ multi_swe_bench/harness/repos/python/__init__.py
@@ -28,3 +28,5 @@ from multi_swe_bench.harness.repos.python.omry import *
 from multi_swe_bench.harness.repos.python.feature_engine import *
 from multi_swe_bench.harness.repos.python.haddocking import *
 from multi_swe_bench.harness.repos.python.conan_io import *
+from multi_swe_bench.harness.repos.python.Deltares import *
+from multi_swe_bench.harness.repos.python.stfc import *
