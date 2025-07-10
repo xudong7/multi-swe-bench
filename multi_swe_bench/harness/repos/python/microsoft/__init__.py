2c5a17b8fdd9ffed36ec2c5dda8da28d0b7609fa
aoyanli <aoyanli.722@bytedance.com>
2025-07-10 10:22:10 +0000

    Update files by envagent

---
+++ multi_swe_bench/harness/repos/python/microsoft/__init__.py
@@ -0,0 +1,4 @@
+from multi_swe_bench.harness.repos.python.microsoft.electionguard_python_1_2_1 import *
+from multi_swe_bench.harness.repos.python.microsoft.electionguard_python_1_2_3 import *
+from multi_swe_bench.harness.repos.python.microsoft.electionguard_python_1_2_2 import *
+from multi_swe_bench.harness.repos.python.microsoft.electionguard_python_1_3_0 import *
