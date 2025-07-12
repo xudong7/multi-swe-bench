13278436b86d8c3beeea07d496551f852118d14d
aoyanli <aoyanli.722@bytedance.com>
2025-07-12 11:49:03 +0000

    Update files by envagent

--- multi_swe_bench/harness/repos/python/__init__.py
+++ multi_swe_bench/harness/repos/python/__init__.py
@@ -30,3 +30,7 @@ from multi_swe_bench.harness.repos.python.haddocking import *
 from multi_swe_bench.harness.repos.python.conan_io import *
 from multi_swe_bench.harness.repos.python.Deltares import *
 from multi_swe_bench.harness.repos.python.stfc import *
+from multi_swe_bench.harness.repos.python.bokeh import *
+from multi_swe_bench.harness.repos.python.RDFLib import *
+from multi_swe_bench.harness.repos.python.missionpinball import *
+from multi_swe_bench.harness.repos.python.debrief import *
