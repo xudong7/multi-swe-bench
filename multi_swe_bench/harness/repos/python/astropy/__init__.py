08c5215d94db402fb49bd4c7d02d720bcc0804d2
aoyanli <aoyanli.722@bytedance.com>
2025-08-06 11:19:56 +0800

    fix org astropy

---
+++ multi_swe_bench/harness/repos/python/astropy/__init__.py
@@ -0,0 +1,6 @@
+from multi_swe_bench.harness.repos.python.astropy.astropy import *
+from multi_swe_bench.harness.repos.python.astropy.ccdproc_2_1_0 import *
+from multi_swe_bench.harness.repos.python.astropy.ccdproc_2_1_1 import *
+from multi_swe_bench.harness.repos.python.astropy.ccdproc_v1_0 import *
+from multi_swe_bench.harness.repos.python.astropy.ccdproc_v1_2_0 import *
+
