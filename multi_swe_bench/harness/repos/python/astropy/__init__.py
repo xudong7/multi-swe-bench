fd8ef1539aecccc5b29250891334bdbfc1b91c97
aoyanli <aoyanli.722@bytedance.com>
2025-08-06 11:41:47 +0800

    add ccdproc v220

--- multi_swe_bench/harness/repos/python/astropy/__init__.py
+++ multi_swe_bench/harness/repos/python/astropy/__init__.py
@@ -1,6 +1,7 @@
 from multi_swe_bench.harness.repos.python.astropy.astropy import *
 from multi_swe_bench.harness.repos.python.astropy.ccdproc_2_1_0 import *
 from multi_swe_bench.harness.repos.python.astropy.ccdproc_2_1_1 import *
+from multi_swe_bench.harness.repos.python.astropy.ccdproc_2_2_0 import *
 from multi_swe_bench.harness.repos.python.astropy.ccdproc_v1_0 import *
 from multi_swe_bench.harness.repos.python.astropy.ccdproc_v1_2_0 import *
 
